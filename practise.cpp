5607a7a224c2ab202bc99d9ea5c24f92ce5a2f50
SAGAR VERMA <vermasagar052004@gmail.com>
2023-12-25 03:51:37 +0530

    version 2

--- practise.cpp
+++ practise.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main (){
-    cout << "hello world" << endl;
+    cout << "sagar verma" << endl;
     return 0;
 }
--- tut2.cpp
+++ tut2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main (){
-    cout << 0 << endl;
+    cout << 1 << endl;
     return 0;
 }
